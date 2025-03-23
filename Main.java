474f45271f07aa34aa3ac18ff985ebb54ae99cef
sqrt1k <51130175+sqrt1k@users.noreply.github.com>
2025-03-24 00:53:56 +0300

    Add files via upload
    Сделал выбор сортировки, немного исправил конструктор

--- Main.java
+++ Main.java
@@ -1,47 +1,63 @@
 import java.lang.reflect.Array;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Sort sort = new Sort();
-        sort.bubllesort(sort.massiv);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Выберите нужную сортировку: ");
+        System.out.println("1 - Сортировка пузырьком");
+
+        int choise = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Введите количество элементов в массиве: ");
+        int count = scanner.nextInt();
+        switch (choise){
+            case 1:
+                System.out.println("Сортировка пузырьком");
+                Sort sort = new Sort(count);
+                sort.bubllesort();
+        }
     }
 }
 
 class Sort {
-    int count = 30;
-    int[] massiv = new int[count];
+    private int count;
+    private int[] massiv;
 
-    Sort() {
+    //В конструкторе создаём массив и заполняем случайными элементами
+    Sort(int count) {
+        this.count = count;
+        massiv = new int[count];
         Random random = new Random();
         for (int i = 0; i < count; i++) {
-            massiv[i] = (int) (random.nextInt(1000));
+            massiv[i] = (int)(random.nextInt(1000));
         }
         display(massiv);
     }
 
-    void bubllesort(int[] array) {
+    public void bubllesort() {
         int temp = 0;
         for (int i = 0; i < count; i++) {
             for (int j = 0; j < count - 1; j++) {
-                if (array[j] > array[j + 1]) {
-                    temp = array[j];
-                    array[j] = array[j + 1];
-                    array[j + 1] = temp;
-                    display(array);
+                if (massiv[j] > massiv[j + 1]) {
+                    temp = massiv[j];
+                    massiv[j] = massiv[j + 1];
+                    massiv[j + 1] = temp;
+                    display(massiv);
                 }
             }
         }
     }
 
-    void display(int[] massiv) {
+    private void display(int[] massiv) {
         for (int i = 0; i < count; i++) {
             System.out.print(massiv[i] + " ");
         }
         System.out.println(" ");
         try {
             // Приостановка выполнения на 3 секунды (3000 миллисекунд)
-            Thread.sleep(250);
+            Thread.sleep(200);
         } catch (InterruptedException e) {
         }
     }
